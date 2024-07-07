094e14fee1f51a4405f8d65709d5683e8cfb3595
ben-smyth <ben.df.smyth@gmail.com>
2024-07-07 12:52:44 +0100

    working timeline, which needs refinement

--- web/static/scripts/animation.js
+++ web/static/scripts/animation.js
@@ -16,3 +16,14 @@ window.addEventListener('scroll', function() {
         button.style.display = 'none';
     }
 });
+
+function toggleText(button) {
+    const content = button.previousElementSibling;
+    if (content.style.display === "none" || content.style.display === "") {
+      content.style.display = "block";
+        button.textContent = "-";
+    } else {
+      content.style.display = "none";
+        button.textContent = "+";
+    }
+}
